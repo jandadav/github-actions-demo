749237a3d8ac76ae8712aa658f182a9d8eb9ac95
jandadav <janda.david@gmail.com>
2021-03-21 20:42:05 +0100

    initial

---
+++ src/main/groovy/com/jandadav/ghActonsDemo/GhActonsDemoApplication.groovy
@@ -0,0 +1,13 @@
+package com.jandadav.ghActonsDemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GhActonsDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GhActonsDemoApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/jandadav/ghActonsDemo/GhActonsDemoApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.jandadav.ghActonsDemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GhActonsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
